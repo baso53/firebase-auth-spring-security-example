6310e979dad19bfcfa1b61c9a603a1c081bb9e88
Sebastijan Grabar <s.grabar@reply.de>
2021-11-12 17:29:08 +0100

    Initial commit

---
+++ src/main/java/com/firebaseauth/springsecurity/part1/Part1Application.java
@@ -0,0 +1,13 @@
+package com.firebaseauth.springsecurity.part1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/firebaseauth/springsecurity/part1/Part1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firebaseauth.springsecurity.part1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
