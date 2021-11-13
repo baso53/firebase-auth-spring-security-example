93a3ea66af576d5b5c7baa8e73d065f7fd293d98
Sebastijan Grabar <s.grabar@reply.de>
2021-11-13 22:53:40 +0100

    Modify config to support method level security and disable CSRF check

--- src/main/java/com/firebaseauth/springsecurity/part2/config/WebSecurityConfiguration.java
+++ src/main/java/com/firebaseauth/springsecurity/part2/config/WebSecurityConfiguration.java
@@ -1,17 +1,15 @@
 package com.firebaseauth.springsecurity.part2.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated();
+        http.csrf().disable();
 
         http.oauth2ResourceServer()
                 .jwt();
