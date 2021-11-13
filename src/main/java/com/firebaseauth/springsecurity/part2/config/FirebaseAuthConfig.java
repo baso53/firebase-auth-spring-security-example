20a9d74d4e49fe1a4ef254c89eb1bedbae4e74c6
Sebastijan Grabar <s.grabar@reply.de>
2021-11-13 22:09:53 +0100

    Add FirebaseAuthConfig

---
+++ src/main/java/com/firebaseauth/springsecurity/part2/config/FirebaseAuthConfig.java
@@ -0,0 +1,30 @@
+package com.firebaseauth.springsecurity.part2.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseAuthConfig {
+
+    @Value("classpath:service-account.json")
+    Resource serviceAccount;
+
+    @Bean
+    FirebaseAuth firebaseAuth() throws IOException {
+        var options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .build();
+
+        var firebaseApp = FirebaseApp.initializeApp(options);
+
+        return FirebaseAuth.getInstance(firebaseApp);
+    }
+}
