9e7a7fac35b0ede4e37f0f5d388465ad657008ff
Sebastijan Grabar <s.grabar@reply.de>
2021-11-13 22:53:59 +0100

    Add endpoint to set custom claims for a user

---
+++ src/main/java/com/firebaseauth/springsecurity/part2/admin/AdminController.java
@@ -0,0 +1,27 @@
+package com.firebaseauth.springsecurity.part2.admin;
+
+import com.firebaseauth.springsecurity.part2.security.Permission;
+import com.google.firebase.auth.FirebaseAuthException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final UserManagementService userManagementService;
+
+    @Secured("ROLE_ANONYMOUS")
+    @PostMapping(path = "/user-claims/{uid}")
+    public void setUserClaims(
+            @PathVariable String uid,
+            @RequestBody List<Permission> requestedClaims
+    ) throws FirebaseAuthException {
+        userManagementService.setUserClaims(uid, requestedClaims);
+    }
+
+}
