1c3cbfe5412dcb0eda9ecac90859ca274313237b
Sebastijan Grabar <s.grabar@reply.de>
2021-11-13 23:19:15 +0100

    Add @PreAuthorize

--- src/main/java/com/firebaseauth/springsecurity/part2/app/AppController.java
+++ src/main/java/com/firebaseauth/springsecurity/part2/app/AppController.java
@@ -1,5 +1,6 @@
 package com.firebaseauth.springsecurity.part2.app;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.security.Principal;
 public class AppController {
 
     @GetMapping(path = "/test")
+    @PreAuthorize("hasAuthority('READ')")
     public String test(Principal principal) {
         return principal.getName();
     }
