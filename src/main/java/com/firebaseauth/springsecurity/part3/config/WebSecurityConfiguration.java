19a2123b2a731cdc861c2d2a3096a5b68bb60b5d
Sebastijan Grabar <s.grabar@reply.de>
2021-11-14 14:06:36 +0100

    Modify security config to support domain object authorization

--- src/main/java/com/firebaseauth/springsecurity/part3/config/WebSecurityConfiguration.java
+++ src/main/java/com/firebaseauth/springsecurity/part3/config/WebSecurityConfiguration.java
@@ -1,14 +1,17 @@
 package com.firebaseauth.springsecurity.part3.config;
 
+import com.firebaseauth.springsecurity.part3.security.DomainGrantedAuthority;
+import com.firebaseauth.springsecurity.part3.security.EntityType;
+import com.firebaseauth.springsecurity.part3.security.Permission;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -29,9 +32,24 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 Optional.ofNullable(jwt.getClaimAsStringList("custom_claims"))
                         .stream()
                         .flatMap(Collection::stream)
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList())
-        );
+                        .flatMap(claim -> {
+                            var parts = claim.split(":", 3);
+
+                            EntityType entityType;
+                            Long entityId;
+                            Permission permission;
+
+                            try {
+                                entityType = EntityType.valueOf(parts[0]);
+                                entityId = Long.valueOf(parts[1]);
+                                permission = Permission.valueOf(parts[2]);
+                            } catch (IllegalArgumentException e) {
+                                return Stream.empty();
+                            }
+
+                            return Stream.of(new DomainGrantedAuthority(entityType, entityId, permission));
+                        })
+                        .collect(Collectors.toList()));
 
         return converter;
     }
