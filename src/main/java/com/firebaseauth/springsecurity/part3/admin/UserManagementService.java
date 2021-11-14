0f219ab89f376b2bf54a5090b74eb1eef1e697e8
Sebastijan Grabar <s.grabar@reply.de>
2021-11-14 13:38:46 +0100

    Modify setTokenClaims to accept a map for ACL

--- src/main/java/com/firebaseauth/springsecurity/part3/admin/UserManagementService.java
+++ src/main/java/com/firebaseauth/springsecurity/part3/admin/UserManagementService.java
@@ -1,14 +1,15 @@
 package com.firebaseauth.springsecurity.part3.admin;
 
+import com.firebaseauth.springsecurity.part3.security.EntityType;
 import com.firebaseauth.springsecurity.part3.security.Permission;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -16,14 +17,29 @@ public class UserManagementService {
 
     private final FirebaseAuth firebaseAuth;
 
-    public void setUserClaims(String uid, List<Permission> requestedPermissions) throws FirebaseAuthException {
-        List<String> permissions = requestedPermissions
-                .stream()
-                .map(Enum::toString)
-                .collect(Collectors.toList());
-
-        Map<String, Object> claims = Map.of("custom_claims", permissions);
+    public void setTokenClaims(String uid, Map<EntityType, Map<Long, Set<Permission>>> requestedPermissions) throws FirebaseAuthException {
+        var claims = toUserClaims(requestedPermissions);
 
         firebaseAuth.setCustomUserClaims(uid, claims);
+    }
+
+    private Map<String, Object> toUserClaims(Map<EntityType, Map<Long, Set<Permission>>> requestedPermissions) {
+        var customClaims = new ArrayList<String>();
+
+        requestedPermissions.forEach((entityType, entityTypePermissions) ->
+                entityTypePermissions.forEach((entityId, permissions) ->
+                        permissions.forEach(permission ->
+                                customClaims.add(generateClaim(entityType, entityId, permission))
+                        )));
+
+        return Map.of("custom_claims", customClaims);
+    }
+
+    private String generateClaim(EntityType entityType, Long entityId, Permission permission) {
+        return entityType +
+                ":" +
+                entityId +
+                ":" +
+                permission;
     }
 }
