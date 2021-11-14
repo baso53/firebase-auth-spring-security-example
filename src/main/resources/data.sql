4b5b65c0eaef3bdd9a5c26211880aa77393022c3
Sebastijan Grabar <s.grabar@reply.de>
2021-11-14 14:51:48 +0100

    Modify data.sql a bit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 INSERT INTO COMPANY(ID, NAME, TYPE) VALUES(1, 'Imaginary Solutions', 'LLC');
+INSERT INTO COMPANY(ID, NAME, TYPE) VALUES(2, 'Green Innovations', 'LLC');
 
 INSERT INTO SUBSIDIARY(ID, NAME, CITY, COMPANY_ID) VALUES(1, 'Imaginary Solutions California', 'Palo Alto', 1);
 INSERT INTO SUBSIDIARY(ID, NAME, CITY, COMPANY_ID) VALUES(2, 'Imaginary Solutions Texas', 'Austin', 1);
+INSERT INTO SUBSIDIARY(ID, NAME, CITY, COMPANY_ID) VALUES(3, 'Green Innovations Canada', 'Quebec', 2);
